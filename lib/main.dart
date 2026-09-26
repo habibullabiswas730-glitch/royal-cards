4fa219eb4169b7760da02f197cdaae153e600f88
habibullabiswas730-glitch <habibullabiswas730@gmail.com>
2026-09-26 17:44:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class Deck {
 }
 
 class AppState extends ChangeNotifier {
-  final FirebaseService fb = FirebaseService.instance;
+  final fb = FirebaseService();
   int coins=517, points=1280; String player='player'; String email='', phone=''; bool loggedIn=false, admin=false;
   final List<String> activity=['Welcome bonus +517'];
   String? uid;
